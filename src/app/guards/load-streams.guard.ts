1fe93b8ea7b1f6facc037a9eefecab90d5964d83
GVabal <gvabal@gmail.com>
2021-03-15 17:41:26 +0200

    Fix guards

--- src/app/guards/load-streams.guard.ts
+++ src/app/guards/load-streams.guard.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
-import {combineLatest, Observable, of} from 'rxjs';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import {combineLatest, Observable} from 'rxjs';
 import {Store} from '@ngrx/store';
 import {loadStreams} from '../store/stream/stream.actions';
-import {filter, take, tap, switchMap} from 'rxjs/operators';
+import {take, tap, mapTo} from 'rxjs/operators';
 import {getIsStreamLoaded, getIsStreamLoading} from '../store/stream/stream.selectors';
 
 @Injectable({
@@ -14,16 +14,19 @@ export class LoadStreamsGuard implements CanActivate {
 
   canActivate(
     route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    state: RouterStateSnapshot): Observable<boolean> {
 
     const isStreamsLoading$ = this.store.select(getIsStreamLoading);
     const isStreamsLoaded$ = this.store.select(getIsStreamLoaded);
 
     return combineLatest([isStreamsLoading$, isStreamsLoaded$]).pipe(
-      filter(([loading, loaded]) => !loading && !loaded),
-      take(1),
-      tap(() => this.store.dispatch(loadStreams())),
-      switchMap(() => of(true))
+      tap(([isStreamsLoading, isStreamLoaded]) => {
+        if (!isStreamsLoading && !isStreamLoaded) {
+          this.store.dispatch(loadStreams());
+        }
+      }),
+      mapTo(true),
+      take(1)
     );
   }
 }
--- src/app/guards/load-students.guard.ts
+++ src/app/guards/load-students.guard.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
-import {combineLatest, Observable, of} from 'rxjs';
-import {filter, switchMap, take, tap} from 'rxjs/operators';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import {combineLatest, Observable} from 'rxjs';
+import {mapTo, take, tap} from 'rxjs/operators';
 import {loadStudents} from '../store/students/students.actions';
 import {Store} from '@ngrx/store';
 import {selectIsStudentsLoaded, selectIsStudentsLoading} from '../store/students/students.selectors';
@@ -14,16 +14,19 @@ export class LoadStudentsGuard implements CanActivate {
 
   canActivate(
     route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    state: RouterStateSnapshot): Observable<boolean> {
 
     const isStudentsLoading$ =  this.store.select(selectIsStudentsLoading);
     const isStudentsLoaded$ = this.store.select(selectIsStudentsLoaded);
 
     return combineLatest([isStudentsLoading$, isStudentsLoaded$]).pipe(
-      filter(([loading, loaded]) => !loading && !loaded),
-      take(1),
-      tap(() => this.store.dispatch(loadStudents())),
-      switchMap(() => of(true))
+    tap(([isStudentsLoading, isStudentsLoaded]) => {
+      if (!isStudentsLoading && !isStudentsLoaded) {
+        this.store.dispatch(loadStudents());
+      }
+    }),
+      mapTo(true),
+      take(1)
   );
   }
 }
